c0db6bfdb6e6d54b9c270be07b084885369e6ff1
iron9light <iron9light@gmail.com>
2011-08-26 12:19:54 +0800

    Fixed issue 2

--- src/main/java/iron9light/coffeescriptMavenPlugin/CoffeeScriptWatchMojo.java
+++ src/main/java/iron9light/coffeescriptMavenPlugin/CoffeeScriptWatchMojo.java
@@ -17,32 +17,39 @@ public class CoffeeScriptWatchMojo extends CoffeeScriptMojoBase {
      */
     private Boolean allowedDelete;
 
-    @Override protected void doExecute(CoffeeScriptCompiler compiler, Path sourceDirectory, Path outputDirectory) throws Exception {
+    @Override
+    protected void doExecute(CoffeeScriptCompiler compiler, Path sourceDirectory, Path outputDirectory) throws Exception {
         compileCoffeeFilesInDir(compiler, sourceDirectory, outputDirectory);
         watch(compiler, sourceDirectory, outputDirectory);
     }
 
     private void watch(final CoffeeScriptCompiler compiler, final Path sourceDirectory, final Path outputDirectory) throws IOException, InterruptedException {
         WatchService watchService = startWatching(sourceDirectory);
-        for(;;) {
-            getLog().info("wait for changing...");
+        for (boolean changed = true; ; ) {
+            if (changed) {
+                getLog().info("wait for changing...");
+                changed = false;
+            }
+
             WatchKey watchKey = watchService.take();
 
-            for(WatchEvent<?> event: watchKey.pollEvents()) {
-                Path file = (Path)event.context();
-                if(Files.isDirectory(file) || !isCoffeeFile(file)) {
+            for (WatchEvent<?> event : watchKey.pollEvents()) {
+                Path file = (Path) event.context();
+                if (Files.isDirectory(file) || !isCoffeeFile(file)) {
                     continue;
+                } else {
+                    changed = true;
                 }
 
                 String coffeeFileName = file.toString();
                 String jsFileName = getJsFileName(coffeeFileName);
                 Path jsFile = outputDirectory.resolve(jsFileName);
 
-                if(event.kind().name().equals(StandardWatchEventKinds.ENTRY_DELETE.name())) {
+                if (event.kind().name().equals(StandardWatchEventKinds.ENTRY_DELETE.name())) {
                     if (Files.deleteIfExists(jsFile)) {
                         getLog().info(String.format("deleted %s with %s", jsFileName, coffeeFileName));
                     }
-                } else if(event.kind().name().equals(StandardWatchEventKinds.ENTRY_MODIFY.name()) || event.kind().name().equals(StandardWatchEventKinds.ENTRY_CREATE.name())) {
+                } else if (event.kind().name().equals(StandardWatchEventKinds.ENTRY_MODIFY.name()) || event.kind().name().equals(StandardWatchEventKinds.ENTRY_CREATE.name())) {
                     compileCoffeeFile(compiler, sourceDirectory.resolve(file), jsFile, coffeeFileName, jsFileName);
                 }
             }
